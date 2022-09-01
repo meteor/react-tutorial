36dea7f226abe82b7db9c0b8b6688b0ee056cfe8
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-09-01 10:00:45 -0300

    chore(step07): updated server

--- src/simple-todos/step07/server/main.js
+++ src/simple-todos/step07/server/main.js
@@ -2,8 +2,8 @@ import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 import { TasksCollection } from '/imports/api/TasksCollection';
 
-const insertTask = (taskText, user) =>
-  TasksCollection.insert({
+const insertTask = async (taskText, user) =>
+  await TasksCollection.insertAsync({
     text: taskText,
     userId: user._id,
     createdAt: new Date(),
@@ -12,7 +12,7 @@ const insertTask = (taskText, user) =>
 const SEED_USERNAME = 'meteorite';
 const SEED_PASSWORD = 'password';
 
-Meteor.startup(() => {
+Meteor.startup( async () => {
   if (!Accounts.findUserByUsername(SEED_USERNAME)) {
     Accounts.createUser({
       username: SEED_USERNAME,
@@ -21,8 +21,7 @@ Meteor.startup(() => {
   }
 
   const user = Accounts.findUserByUsername(SEED_USERNAME);
-
-  if (TasksCollection.find().count() === 0) {
+  if (await TasksCollection.find().countAsync() === 0) {
     [
       'First Task',
       'Second Task',
@@ -31,6 +30,6 @@ Meteor.startup(() => {
       'Fifth Task',
       'Sixth Task',
       'Seventh Task',
-    ].forEach(taskText => insertTask(taskText, user));
+    ].forEach(insertTask);
   }
 });
