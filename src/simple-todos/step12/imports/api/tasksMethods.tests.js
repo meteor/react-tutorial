db0769a379e3ea97213b8dfa01f6fc1402f9fbc9
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-09-01 15:57:08 -0300

    chore(step12): updated taskMethod.tests.js

--- src/simple-todos/step12/imports/api/tasksMethods.tests.js
+++ src/simple-todos/step12/imports/api/tasksMethods.tests.js
@@ -7,57 +7,57 @@ import '/imports/api/tasksMethods';
 
 if (Meteor.isServer) {
   describe('Tasks', () => {
-    describe('methods', () => {
+    describe('methods', async () => {
       const userId = Random.id();
       let taskId;
 
-      beforeEach(() => {
-        TasksCollection.remove({});
-        taskId = TasksCollection.insert({
+      beforeEach(async () => {
+        await TasksCollection.removeAsync({});
+        taskId = await TasksCollection.insertAsync({
           text: 'Test Task',
           createdAt: new Date(),
           userId,
         });
       });
 
-      it('can delete owned task', () => {
+      it('can delete owned task', async () => {
         mockMethodCall('tasks.remove', taskId, { context: { userId } });
 
-        assert.equal(TasksCollection.find().count(), 0);
+        assert.equal(await TasksCollection.find().countAsync(), 0);
       });
 
-      it('can\'t delete task without an user authenticated', () => {
+      it('can\'t delete task without an user authenticated', async () => {
         const fn = () => mockMethodCall('tasks.remove', taskId);
         assert.throw(fn, /Not authorized/);
-        assert.equal(TasksCollection.find().count(), 1);
+        assert.equal(await TasksCollection.find().countAsync(), 1);
       });
 
-      it('can\'t delete task from another owner', () => {
+      it('can\'t delete task from another owner', async () => {
         const fn = () =>
           mockMethodCall('tasks.remove', taskId, {
             context: { userId: 'somebody-else-id' },
           });
         assert.throw(fn, /Access denied/);
-        assert.equal(TasksCollection.find().count(), 1);
+        assert.equal(await TasksCollection.find().countAsync(), 1);
       });
 
-      it('can change the status of a task', () => {
-        const originalTask = TasksCollection.findOne(taskId);
+      it('can change the status of a task', async () => {
+        const originalTask = await TasksCollection.findOneAsync(taskId);
         mockMethodCall('tasks.setIsChecked', taskId, !originalTask.isChecked, {
           context: { userId },
         });
 
-        const updatedTask = TasksCollection.findOne(taskId);
+        const updatedTask = await TasksCollection.findOneAsync(taskId);
         assert.notEqual(updatedTask.isChecked, originalTask.isChecked);
       });
 
-      it('can insert new tasks', () => {
+      it('can insert new tasks', async () => {
         const text = 'New Task';
         mockMethodCall('tasks.insert', text, {
           context: { userId },
         });
 
-        const tasks = TasksCollection.find({}).fetch();
+        const tasks = await TasksCollection.find({}).fetchAsync();
         assert.equal(tasks.length, 2);
         assert.isTrue(tasks.some(task => task.text === text));
       });
