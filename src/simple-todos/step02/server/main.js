90b001be0ae6d4d53490126a51b12011fee9a823
Jacob Gilbert <jacobkgilbert@protonmail.com>
2024-08-27 11:59:11 -0500

    Update main.js - countAsync()
    When following the tutorial I ran into an issue where the app kept crashing after completing this step. The fix for this was changing "count()" to "countAsync()"

--- src/simple-todos/step02/server/main.js
+++ src/simple-todos/step02/server/main.js
@@ -4,7 +4,7 @@ import { TasksCollection } from '/imports/api/TasksCollection';
 const insertTask = taskText => TasksCollection.insert({ text: taskText });
 
 Meteor.startup(() => {
-  if (TasksCollection.find().count() === 0) {
+  if (TasksCollection.find().countAsync() === 0) {
     [
       'First Task',
       'Second Task',
